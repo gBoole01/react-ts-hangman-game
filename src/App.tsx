125803a8c6eae2f2badfdcc7cd6683c6c87321ac
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-01-06 20:14:05 +0100

    feat: add wordList and HangmanDrawing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
-import './App.css'
+import { useState } from 'react'
+import { HangmanDrawing } from './HangmanDrawing'
+import { HangmanWord } from './HangmanWord'
+import { Keyboard } from './Keyboard'
+import words from './wordList.json'
 
 function App() {
-  return <h1>Hangman</h1>
+  const [wordToGuess, setWordToGuess] = useState((): string => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+  return (
+    <div
+      style={{
+        maxWidth: '800px',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '2rem',
+        margin: '0 auto',
+        alignItems: 'center',
+      }}
+    >
+      <div style={{ fontSize: '2rem', textAlign: 'center' }}>Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  )
 }
 
 export default App
